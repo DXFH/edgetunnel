5099a3360a7a7948acae5fe1baccdb985780a6de
DXFH <65805333+DXFH@users.noreply.github.com>
2024-12-15 13:45:30 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1,7 +1,7 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
+let userID = '1e8a8d11-25a9-41fd-bf59-18c777b2e82a';
 let proxyIP = '';
 let sub = '';
 let subConverter = 'SUBAPI.fxxk.dedyn.io';
